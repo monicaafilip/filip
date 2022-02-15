7676a680ded7fc120786db753c1e7b364d73439c
Monica <32903674+monicaafilip@users.noreply.github.com>
2022-02-15 18:18:09 +0200

    updated info about the company + updated logo

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -5,46 +5,57 @@ const Services = () => {
     <section className="page-section" id="services">
       <div className="container">
         <div className="text-center">
-          <h2 className="section-heading text-uppercase">Servicii</h2>
-          <h3 className="section-subheading text-muted">
-            Lorem ipsum dolor sit amet consectetur.
-          </h3>
+          <h2 className="section-heading text-uppercase">Servicii oferite</h2>
         </div>
         <div className="row text-center">
           <div className="col-md-4">
-            <span className="fa-stack fa-4x">
-              <i className="fas fa-circle fa-stack-2x text-primary"></i>
-              <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-            </span>
-            <h4 className="my-3">E-Commerce</h4>
+            <h4 className="my-3">Instalații sanitare</h4>
             <p className="text-muted">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-              maxime quam architecto quo inventore harum ex magni, dicta
-              impedit.
+              <b>Montaj</b>
+              <ul>
+                <li>obiecte sanitare</li>
+                <li>pompe sumersibile</li>
+                <li>kit filtre de apă potabilă</li>
+                <li>boilere</li>
+                <li>panouri solare</li>
+              </ul>
             </p>
           </div>
           <div className="col-md-4">
-            <span className="fa-stack fa-4x">
-              <i className="fas fa-circle fa-stack-2x text-primary"></i>
-              <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
-            </span>
-            <h4 className="my-3">Responsive Design</h4>
+            <h4 className="my-3">Instalații termice</h4>
             <p className="text-muted">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-              maxime quam architecto quo inventore harum ex magni, dicta
-              impedit.
+              <b>Montaj</b>
+              <ul>
+                <li>centrale pe lemne</li>
+                <li>centrale pe peleți</li>
+                <li>centrale pe gaz</li>
+                <li>puffer cu serpentină</li>
+                <li>panouri solare</li>
+                <li>pompe de căldură aer-apă</li>
+                <li>aer condiționat</li>
+                <li>calorifere</li>
+                <li>încălzire în pardoseală (+industrială)</li>
+                <li>ventilații cu recuperator de căldură</li>
+              </ul>
             </p>
           </div>
           <div className="col-md-4">
-            <span className="fa-stack fa-4x">
-              <i className="fas fa-circle fa-stack-2x text-primary"></i>
-              <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
-            </span>
-            <h4 className="my-3">Web Security</h4>
+            <h4 className="my-3">Instalații electrice</h4>
             <p className="text-muted">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-              maxime quam architecto quo inventore harum ex magni, dicta
-              impedit.
+              <b>Montaj</b>
+              <ul>
+                <li>prize</li>
+                <li>întrerupătoare clasice</li>
+                <li>întrerupătoare cap-scară</li>
+                <li>întrerupătoare cap-cruce</li>
+                <li>pulsanți</li>
+                <li>relee</li>
+                <li>automatizări case (smart-home) prin KNX</li>
+                <li>controlul iluminatului</li>
+                <li>dimming</li>
+                <li>controlul jaluzelelor</li>
+                <li>controlul temperaturii pe zone</li>
+              </ul>
             </p>
           </div>
         </div>
